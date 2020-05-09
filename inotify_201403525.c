3dd846e87d8235ace6260f1434c8df0e3a6dc337
nerygalvez <nerygalvezgomez@gmail.com>
2020-05-08 19:54:46 -0600

    Probando otro código de inotify

--- inotify_201403525.c
+++ inotify_201403525.c
@@ -1,108 +1,116 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
 #include <sys/types.h>
-#include <linux/inotify.h>
-#include <time.h>
-#include <string.h>
-
-#define EVENT_SIZE ( sizeof (struct inotify_event) )
-#define EVENT_BUF_LEN ( 1024 * (EVENT_SIZE) + 16)
-#define RED "\e[31m"
-#define YELLOW "\e[33m"
-#define GREEN "\e[32m"
-#define ENDC "\e[00m"
-FILE * get_log();
-
-int main() {
-  FILE *log = get_log();
-  int length, i;
+#include <sys/inotify.h>
+#include <limits.h>
+ 
+#define MAX_EVENTS 1024 /*Max. number of events to process at one go*/
+#define LEN_NAME 16 /*Assuming that the length of the filename won't exceed 16 bytes*/
+#define EVENT_SIZE  ( sizeof (struct inotify_event) ) /*size of one event*/
+#define BUF_LEN     ( MAX_EVENTS * ( EVENT_SIZE + LEN_NAME )) /*buffer to store the data of events*/
+ 
+int main( int argc, char **argv ) 
+{
+  int length, i = 0, wd;
   int fd;
-  int wd;
-  char buffer[EVENT_BUF_LEN];
-  char h_evento [128] = {0};
-  
-  /*instancia inotify*/
+  char buffer[BUF_LEN];
+ 
+  /* Initialize Inotify*/
   fd = inotify_init();
-  
-  /*checking for error*/
-  if ( fd < 0) {
-    perror ("inotify_init");
+  if ( fd < 0 ) {
+    perror( "No se pudo iniciar inotify");
   }
-    
-    /*agregando directorio*/
-  wd = inotify_add_watch(fd, "/home/wxjoy/Desarrollo/SO2/test", IN_DELETE | IN_CREATE | IN_MODIFY);
-    
-  while(1){
-    /*determina que pasa*/
-    
-    length = read( fd, buffer, EVENT_BUF_LEN);
-    i=0;
-    
-    if( length < 0){
-      perror ("read");
+ 
+  /* add watch to starting directory */
+  wd = inotify_add_watch(fd, "/", IN_CREATE | IN_MODIFY | IN_DELETE); 
+ 
+  if (wd == -1)
+    {
+      printf("NO se pudo agregar reloj %s\n","/");
     }
-    
-    /*leyendo la lista de eventos*/
-    time_t t = time(0);
-    struct tm *tlocal = localtime (&t);
-    while( i < length){
-      struct inotify_event *event = ( struct inotify_event *)&buffer[i];
-      if( event->len){
-        
-        strftime (h_evento, 128, "%d/%m/%Y %H:%M:%S", tlocal);
-        if( event->mask & IN_DELETE){
-          printf("%s", RED);
-          if( event->mask & IN_ISDIR){
-            printf ("%s, se ha borrado el directorio: %s\n", h_evento, event->name);
-            fprintf (log, "%s, Se ha borrado el directorio: %s\n", h_evento, event->name);
-          }else{
-            printf ("%s, se ha borrado el archivo: %s\n", h_evento, event->name);
-            fprintf (log, "%s, se ha borrado el archivo: %s\n", h_evento, event->name);
-          }
-          printf("%s", ENDC);
-        }else if(event->mask & IN_CREATE){
-          printf("%s", GREEN);
-          if( event->mask & IN_ISDIR){
-            printf ("%s, se ha creado el directorio: %s\n",h_evento, event->name);
-            fprintf (log, "%s, se ha creado el directorio: %s\n", h_evento, event->name);
-          }else{
-            printf ("%s, se ha creado el archivo: %s\n", h_evento, event->name);
-            fprintf (log, "%s, se ha creado el archivo: %s\n", h_evento, event->name);
-          }
-          printf("%s", ENDC);
-        }else {
-          printf("%s", YELLOW);
-          if(event->mask & IN_ISDIR){
-            printf ("%s, se ha modificado el directorio: %s\n", h_evento, event->name);
-            fprintf (log, "%s, Se ha modificado el directorio: %s\n", h_evento, event->name);
-          } else{
-            printf ("%s, se ha modificado el archivo: %s\n", h_evento, event->name);
-            fprintf (log, "%s, se ha borrado modificado el archivo: %s\n", h_evento, event->name);
-          }
-          printf("%s", ENDC);
-        }
-      }
-      i += EVENT_SIZE + event->len;
-      fflush (log);
+  else
+    {
+      printf("Watching:: %s\n","/");
     }
-    
-  }           
-  
-  inotify_rm_watch(fd, wd);
-  close(fd);
-  
-}
+ 
+         FILE *fptr;
 
-FILE * get_log(){
-  time_t t = time(0);
-  struct tm *tl = localtime (&t);
-  char output[128];
-  strftime(output,128,"%d-%m-%Y %H:%M:%S", tl);
-  char nom[134]={0};
-  strcat (nom, output);
-  strcat (nom, ".log");
-  printf("%s\n", nom);
-  return fopen (nom, "a+");
+  /* do it forever*/
+  while(1)
+    {
+      i = 0;
+      length = read(fd, buffer, BUF_LEN );  
+ 
+      if ( length < 0 ) {
+        perror( "read" );
+      }  
+ 
+      while ( i < length ) {
+
+
+                fptr = fopen("/var/proyecto/201222673_files.log","a");
+
+        
+
+                          if(fptr == NULL)
+                {
+                    printf("Error!");   
+                    return -1;           
+                }
+
+              //int num = 300;
+                  
+                   
+        struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
+        if ( event->len ) {
+          if ( event->mask & IN_CREATE) {
+            if (event->mask & IN_ISDIR)
+              {
+                printf( "El directorio %s fue creado.\n", event->name );     
+               fprintf(fptr,"El directorio %s fue creado.\n", event->name);  
+              }
+            else
+              {
+                printf( "El archivo %s fue creado con WD %d\n", event->name, event->wd );  
+              fprintf(fptr,"El archivo %s fue creado con WD %d\n", event->name, event->wd);   
+              }   
+          }
+           
+          if ( event->mask & IN_MODIFY) {
+            if (event->mask & IN_ISDIR){
+              printf( "El directorio %s fue modificado.\n", event->name );    
+              fprintf(fptr,"El directorio %s fue modificado.\n", event->name );     
+            }
+            else{
+              printf( "El archivo %s fue modificado. %d\n", event->name, event->wd );   
+              fprintf(fptr,"El archivo %s fue modificado.%d\n", event->name, event->wd );  
+            }   
+          }
+           
+          if ( event->mask & IN_DELETE) {
+            if (event->mask & IN_ISDIR){
+              printf( "El directorio %s fue eliminado.\n", event->name );   
+              fprintf(fptr,"El directorio %s fue eliminado.\n", event->name  ); 
+            }    
+            else{
+              printf( "El archivo %s fue eliminado.  %d\n", event->name, event->wd );     
+              fprintf(fptr,"El archivo %s fue eliminado.  %d\n", event->name, event->wd  );  
+            }
+          }  
+ 
+ 
+          i += EVENT_SIZE + event->len;
+          
+        }
+        fclose(fptr);
+      }
+    }
+ 
+  /* Clean up*/
+  inotify_rm_watch( fd, wd );
+  close( fd );
+   
+  return 0;
 }
